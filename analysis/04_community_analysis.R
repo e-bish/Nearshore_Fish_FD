676956385f655b95c5657224eda4d296f456fb7d
Emily Bishop <ebish@uw.edu>
2025-06-29 15:18:06 -0700

    brought back the nmds

--- analysis/04_community_analysis.R
+++ analysis/04_community_analysis.R
@@ -91,63 +91,156 @@ combined_points %>%
 
 #### rda for site ####
 
-fish_L_full <- fish_L_full %>% 
+fish_L_all <- fish_L_full %>% 
   mutate(ipa = ifelse(ipa == "Natural2", "Natural", ipa)) %>% 
   mutate(site = factor(site, levels = c("FAM", "TUR", "COR", "SHR", "DOK", "EDG")),
          ipa = factor(ipa, levels = c("Natural",
                                       "Armored",
                                       "Restored")))
 
-fish_dist <- vegdist(sqrt(fish_L_full[4:45]), method = "bray")
+fish_dist <- vegdist(sqrt(fish_L_all[4:45]), method = "bray")
 
-mod1 <- dbrda(fish_dist ~ ipa + site + year, 
-              data = fish_L_full)
-
-anova(mod1, by = "margin", model = "reduced", permutations = 9999)
-
-pairwise.adonis2(fish_dist ~ site, 
-                 data = fish_L_full, by = "margin")
-
-site_beta <- betadisper(vegdist(fish_dist), 
-                     group = fish_L_full$site, type = "median")
-
-permutest(site_beta, pairwise = TRUE)
+# mod1 <- dbrda(fish_dist ~ ipa + site + year, 
+#               data = fish_L_full)
+# 
+# anova(mod1, by = "margin", model = "reduced", permutations = 9999)
+# 
+# pairwise.adonis2(fish_dist ~ site, 
+#                  data = fish_L_full, by = "margin")
+# 
+# site_beta <- betadisper(vegdist(fish_dist), 
+#                      group = fish_L_full$site, type = "median")
+# 
+# permutest(site_beta, pairwise = TRUE)
 
 #visualize the ordination
-rda_scores1 <- scores(mod1)
-sites_scores1 <- as.data.frame(rda_scores1$sites)
-biplot_scores1 <- as.data.frame(rda_scores1$biplot) %>% 
-  rownames_to_column(var = "fac") %>% 
-  filter(grepl("ipa", fac))
+# rda_scores1 <- scores(mod1)
+# sites_scores1 <- as.data.frame(rda_scores1$sites)
+# biplot_scores1 <- as.data.frame(rda_scores1$biplot) %>% 
+#   rownames_to_column(var = "fac") %>% 
+#   filter(grepl("ipa", fac))
+# 
+# points1 <- sites_scores1 %>% 
+#   cbind(fish_L_full %>% select(site:year))
+# 
+# hulls <- points1 %>%
+#   group_by(site) %>% 
+#   slice(chull(dbRDA1,dbRDA2))
+# 
+# ggplot(data = points1, aes(x = dbRDA1, y = dbRDA2)) +
+#   geom_point(aes(color = site, shape = ipa), size = 3) + 
+#   geom_polygon(data = hulls, 
+#                aes(x = dbRDA1, 
+#                    y = dbRDA2, 
+#                    fill = site), 
+#                alpha = 0.2) +
+#   # stat_ellipse(aes(group = site, color = site),
+#   #              linetype = "dashed", show.legend = FALSE) +
+#   # geom_segment(data=biplot_scores1,
+#   #              aes(x = 0, y = 0, xend=dbRDA1, yend=dbRDA2),
+#   #              arrow=arrow(length = unit(0.01, "npc")),
+#   #              lwd=0.75) +
+#   # geom_text(data=biplot_scores1,
+#   #           aes(x=dbRDA1*0.9,
+#   #               y=dbRDA2*0.9,
+#   #               label=fac),
+#   #           nudge_x = c(-0.2, -0.3, -0.2, -0.2),
+#   #           nudge_y = c(0.1, -0.05, 0, 0),
+#   #           size=4) +
+#   scale_color_manual(values = site_colors) +
+#   scale_fill_manual(values = site_colors) +
+#   labs(shape = "Shoreline Condition", fill = "Site", color = "Site") +
+#   theme_classic() 
 
-points1 <- sites_scores1 %>% 
-  cbind(fish_L_full %>% select(site:year))
+nmds <- metaMDS(fish_dist, k= 3, trymax=1000, plot = FALSE)
 
-hulls <- points1 %>%
+points <- data.frame(nmds$points) %>% 
+  cbind(fish_L_all %>% select(site:year)) %>% 
+  mutate(yr_abb = str_sub(year, start = 3))
+
+hulls <- points %>%
   group_by(site) %>% 
-  slice(chull(dbRDA1,dbRDA2))
+  slice(chull(MDS1,MDS2))
 
-ggplot(data = points1, aes(x = dbRDA1, y = dbRDA2)) +
-  geom_point(aes(color = site, shape = ipa), size = 3) + 
-  geom_polygon(data = hulls, 
-               aes(x = dbRDA1, 
-                   y = dbRDA2, 
-                   fill = site), 
-               alpha = 0.2) +
-  # stat_ellipse(aes(group = site, color = site),
-  #              linetype = "dashed", show.legend = FALSE) +
-  # geom_segment(data=biplot_scores1,
-  #              aes(x = 0, y = 0, xend=dbRDA1, yend=dbRDA2),
-  #              arrow=arrow(length = unit(0.01, "npc")),
-  #              lwd=0.75) +
-  # geom_text(data=biplot_scores1,
-  #           aes(x=dbRDA1*0.9,
-  #               y=dbRDA2*0.9,
-  #               label=fac),
-  #           nudge_x = c(-0.2, -0.3, -0.2, -0.2),
-  #           nudge_y = c(0.1, -0.05, 0, 0),
-  #           size=4) +
+nmds_all <- ggplot() +
+  geom_point(data = points, aes(x = MDS1, y = MDS2, color = site, shape = ipa), size = 3) + 
+  geom_text_repel(data = points, 
+                  aes(x = MDS1, y = MDS2, color = site, label = yr_abb), 
+                  max.overlaps = 20, show.legend = FALSE) +
+  # geom_polygon(data = hulls, aes(x = MDS1, y = MDS2, fill = site), alpha = 0.2) +
+  # annotate("text", x = Inf, y = Inf, label = paste("stress = ", S), vjust = 2, hjust = 2) +
+  # annotate("text", x = Inf, y = Inf, label = paste("k = ", K), vjust = 2, hjust = 2) +
+  # geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2), 
+  #              data = wq_vecs, linewidth =1) +
+  # geom_text(data = wq_vecs, aes(x = NMDS1, y = NMDS2), colour = "grey30", 
+  #           fontface = "bold", label = row.names(wq_vecs)) +
   scale_color_manual(values = site_colors) +
   scale_fill_manual(values = site_colors) +
-  labs(shape = "Shoreline Condition", fill = "Site", color = "Site") +
-  theme_classic() 
+  theme_classic() +
+  labs(color = "Site", shape = "Condition Category")
+
+#### nmds2 ####
+
+wq_tb_exp <- wq_tb %>%
+  pivot_longer(c(secchi_depth_m, do_mg_l, salinity_ppm, temperature), names_to = "metric") %>%
+  group_by(shoreline, year, metric) %>%
+  summarize(mean = round(mean(value, na.rm = TRUE), 2), min = min(value, na.rm = TRUE), max = max(value, na.rm = TRUE)) %>%
+  ungroup() %>% 
+  mutate(site = str_sub(shoreline, 1, 3),
+         ipa = str_sub(shoreline, 4), .before = shoreline) %>% 
+  select(!c(shoreline,min,max)) %>% 
+  pivot_wider(names_from = metric, values_from = mean) %>% 
+  mutate(year = factor(year))
+
+fish_L_abb <- fish_L_full %>% 
+  filter(!year %in% c(2018, 2019)) %>% 
+  full_join(wq_tb_exp) %>% 
+  mutate(ipa = ifelse(ipa == "Natural2", "Natural", ipa)) %>% 
+  mutate(site = factor(site, levels = c("FAM", "TUR", "COR", "SHR", "DOK", "EDG")),
+         ipa = factor(ipa, levels = c("Natural",
+                                      "Armored",
+                                      "Restored")))
+  
+fish_dist_abb <- vegdist(sqrt(fish_L_abb[4:45]), method = "bray")
+
+nmds_abb <- metaMDS(fish_dist_abb, k= 3, trymax=1000, plot = FALSE)
+
+points_abb <- data.frame(nmds_abb$points) %>% 
+  cbind(fish_L_abb %>% select(site:year)) %>% 
+  mutate(yr_abb = str_sub(year, start = 3))
+
+hulls_abb <- points_abb %>%
+  group_by(site) %>% 
+  slice(chull(MDS1,MDS2))
+
+env.test <- envfit(nmds_abb, fish_L_abb[46:49], permutations = 9999)
+env.test 
+
+wq_vecs <- as.data.frame(scores(env.test, "vectors")) * ordiArrowMul(env.test)
+
+wq_vec_df <- wq_vecs %>% 
+  rownames_to_column(var = "wq_metric") %>% 
+  filter(!wq_metric == c("do_mg_l", "salinity_ppm")) %>% 
+  mutate(wq_labels = c("Secchi", "Temp"))
+
+nmds_abb <- ggplot() +
+  geom_point(data = points_abb, aes(x = MDS1, y = MDS2, color = site, shape = ipa), size = 3) + 
+  geom_text_repel(data = points_abb, 
+                  aes(x = MDS1, y = MDS2, color = site, label = yr_abb),
+                  max.overlaps = 20, show.legend = FALSE) +
+  # geom_polygon(data = hulls, aes(x = MDS1, y = MDS2, fill = site), alpha = 0.2) +
+  # annotate("text", x = Inf, y = Inf, label = paste("stress = ", S), vjust = 2, hjust = 2) +
+  # annotate("text", x = Inf, y = Inf, label = paste("k = ", K), vjust = 2, hjust = 2) +
+  geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2),
+               data = wq_vec_df, linewidth =0.75, 
+               arrow = arrow(length = unit(0.25, "cm"))) +
+  geom_text(data = wq_vec_df, aes(x = NMDS1, y = NMDS2), colour = "grey30",
+            label = wq_vec_df$wq_labels, size = 4, nudge_y = -0.05) +
+  scale_color_manual(values = site_colors) +
+  scale_fill_manual(values = site_colors) +
+  theme_classic() +
+  labs(color = "Site", shape = "Condition Category", y = "")
+
+library(patchwork)
+
+nmds_all + nmds_abb + plot_layout(guides = "collect") + plot_annotation(tag_levels = 'A')
